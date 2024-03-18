de9df0bdb8a908ac24c22481f91060641a7e1160
Rishi <barnwal.rishi@gmail.com>
2024-03-18 00:20:48 -0400

    Fixed typo in core.h

--- REngine/src/REngine/Core.h
+++ REngine/src/REngine/Core.h
@@ -15,8 +15,8 @@
 #endif 
 
 #ifdef RE_ENABLE_ASSERTS
-    #define RE_ASSERT(x, ...) { if(!(x)) { RE_ERROR("Assertion Failed: {0}", __VA_ARFS__); __debugbreak(); } }
-    #define RE_CORE_ASSERT(x, ...) { if(!(x)) { RE_CORE_ERROR("Assertion Failed: {0}", __VA_ARFS__); __debugbreak(); } }
+    #define RE_ASSERT(x, ...) { if(!(x)) { RE_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
+    #define RE_CORE_ASSERT(x, ...) { if(!(x)) { RE_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
 #else
     #define RE_ASSERT(x, ...)
     #define RE_CORE_ASSERT(x, ...)
