ac8263700df3e3f73808b3c616771566803169b9
Rishi <barnwal.rishi@gmail.com>
2024-04-24 20:11:08 -0400

    layer

--- REngine/src/REngine/Core/LayerStack.cpp
+++ REngine/src/REngine/Core/LayerStack.cpp
@@ -10,7 +10,10 @@ namespace REngine
     REngine::LayerStack::~LayerStack()
     {
         for (Layer* layer : m_layers)
+        {
+            layer->OnDetach();
             delete layer;
+        }
     }
 
     void REngine::LayerStack::PushLayer(Layer* layer)
