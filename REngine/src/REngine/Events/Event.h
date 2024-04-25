dd938a1e1fc98a06c95c95f4104c2ddb9ae465e6
Rishi <barnwal.rishi@gmail.com>
2024-04-24 20:07:38 -0400

    Remove unnecessary token-pasting operator in EVENT_CLASS_TYPE

--- REngine/src/REngine/Events/Event.h
+++ REngine/src/REngine/Events/Event.h
@@ -46,7 +46,7 @@ namespace REngine
 // macros to define common virtual function implementations
 // these are so handy god damn
 
-#define EVENT_CLASS_TYPE(type) static EventType GetStaticType() { return EventType::##type; }\
+#define EVENT_CLASS_TYPE(type) static EventType GetStaticType() { return EventType::type; }\
                                virtual EventType GetEventType() const override { return GetStaticType(); }\
                                virtual const char* GetName() const override { return #type; }
 
