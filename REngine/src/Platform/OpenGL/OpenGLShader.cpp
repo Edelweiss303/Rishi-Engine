3ce3150f55030895221cbe506e5bd7769de7ca02
Rishi <barnwal.rishi@gmail.com>
2024-06-16 22:04:20 -0400

    Added file reading check to log error when file reading size is -1

--- REngine/src/Platform/OpenGL/OpenGLShader.cpp
+++ REngine/src/Platform/OpenGL/OpenGLShader.cpp
@@ -175,9 +175,18 @@ namespace REngine
             if (size != -1)
             {
                 result.resize(size);
-                in.seekg(0, std::ios::beg);
-                in.read(&result[0], size);
-                in.close();
+                size_t size = in.tellg();
+                if (size != -1)
+                {
+                    result.resize(size);
+                    in.seekg(0, std::ios::beg);
+                    in.read(&result[0], size);
+                    in.close();
+                }
+                else
+                {
+                    RE_CORE_ERROR("Could not read from file '{0}'", filepath);
+                }
             }
             else
             {
