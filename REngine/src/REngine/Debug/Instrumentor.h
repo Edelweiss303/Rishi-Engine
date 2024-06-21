b62cde12cb184bce55d278c02d9b7cc76d9fdd3f
Rishi <barnwal.rishi@gmail.com>
2024-06-21 01:37:22 -0400

    Auto deducing an availabe __FUNCSIG__ definition

--- REngine/src/REngine/Debug/Instrumentor.h
+++ REngine/src/REngine/Debug/Instrumentor.h
@@ -121,4 +121,41 @@ namespace REngine
         std::chrono::time_point<std::chrono::high_resolution_clock> m_StartTimepoint;
         bool m_Stopped;
     };
+
 }
+
+
+#define RE_PROFILE 1
+#if RE_PROFILE
+    // Resolve which function signature macro will be used. Note that this only
+    // is resolved when the (pre)compiler starts, so the syntax highlighting
+    // could mark the wrong one in your editor!
+    #if defined(__GNUC__) || (defined(__MWERKS__) && (__MWERKS__ >= 0x3000)) || (defined(__ICC) && (__ICC >= 600)) || defined(__ghs__)
+        #define RE_FUNC_SIG __PRETTY_FUNCTION__
+    #elif defined(__DMC__) && (__DMC__ >= 0x810)
+        #define RE_FUNC_SIG __PRETTY_FUNCTION__
+    #elif defined(__FUNCSIG__)
+        #define RE_FUNC_SIG __FUNCSIG__
+    #elif (defined(__INTEL_COMPILER) && (__INTEL_COMPILER >= 600)) || (defined(__IBMCPP__) && (__IBMCPP__ >= 500))
+        #define RE_FUNC_SIG __FUNCTION__
+    #elif defined(__BORLANDC__) && (__BORLANDC__ >= 0x550)
+        #define RE_FUNC_SIG __FUNC__
+    #elif defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 199901)
+        #define RE_FUNC_SIG __func__
+    #elif defined(__cplusplus) && (__cplusplus >= 201103)
+        #define RE_FUNC_SIG __func__
+#else
+    #define RE_FUNC_SIG "HZ_FUNC_SIG unknown!"
+#endif
+
+    #define RE_PROFILE_BEGIN_SESSION(name, filepath) ::REngine::Instrumentor::Get().BeginSession(name, filepath)
+    #define RE_PROFILE_END_SESSION() ::REngine::Instrumentor::Get().EndSession()
+    #define RE_PROFILE_SCOPE(name) ::REngine::InstrumentationTimer timer##__LINE__(name);
+    #define RE_PROFILE_FUNCTION() RE_PROFILE_SCOPE(RE_FUNC_SIG)
+
+#else
+    #define RE_PROFILE_BEGIN_SESSION(name, filepath)
+    #define RE_PROFILE_END_SESSION()
+    #define RE_PROFILE_SCOPE(name)
+    #define RE_PROFILE_FUNCTION()
+#endif
